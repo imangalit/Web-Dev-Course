34db5e777b2dc8b58c26812e8911615a0b81ec67
imangalit <temirkhan.imangail@gmail.com>
2021-02-23 22:38:28 +0600

    Lab4

---
+++ lab4/angular-szk6x4-ucalur/src/app/products.ts
@@ -0,0 +1,109 @@
+export const products = [
+  {
+    id: 1,
+    name: "Oculus Quest 2 Advanced All-In-One Virtual Reality Headset — 64 GB ",
+    price: 299,
+    rating: 5,
+    image: 'assets/img/1.jpg',
+    description: "by Oculus",
+    link:
+      "https://www.amazon.com/Oculus-Quest-Advanced-All-One-2/dp/B08F7PTF53/ref=lp_16225016011_1_1"
+  },
+  {
+    id: 2,
+    name: "Super Mario 3D World + Bowser's Fury - Nintendo Switch",
+    price: 59.99,
+    rating: 5,
+    image: 'assets/img/2.jpg',
+    description: "by Nintendo",
+    link:
+      "https://www.amazon.com/Super-Mario-3D-World-Bowsers-Fury/dp/B08H9M7LDY/ref=lp_16225016011_1_2"
+  },
+  {
+    id: 3,
+    name: "Nintendo Switch - Mario Red & Blue Edition - Switch",
+    price: 402.98,
+    rating: 5,
+    image: 'assets/img/3.jpg',
+    description: "by Nintendo",
+    link:
+      "https://www.amazon.com/Nintendo-Switch-Mario-Red-Blue/dp/B08M8YQMH4/ref=lp_16225016011_1_3"
+  },
+  {
+    id: 4,
+    name: "Nintendo Switch with Neon Blue and Neon Red Joy‑Con - HAC-001(-01)",
+    price: 340,
+    rating: 5,
+    image: 'assets/img/4.jpg',
+    description: "by Nintendo",
+    link:
+      "https://www.amazon.com/Nintendo-Switch-Neon-Blue-Joy%E2%80%91/dp/B07VGRJDFY/ref=lp_16225016011_1_4"
+  },
+  {
+    id: 5,
+    name: "Nintendo Switch Pro Controller",
+    price: 55,
+    rating: 5,
+    image: 'assets/img/5.jpg',
+    description: "by Nintendo",
+    link:
+      "https://www.amazon.com/Nintendo-Switch-Pro-Controller/dp/B01NAWKYZ0/ref=lp_16225016011_1_5"
+  },
+  {
+    id: 6,
+    name: "Playstation DualSense Wireless Controller",
+    price: 59,
+    rating: 5,
+    image: 'assets/img/6.jpg',
+    description: "by Playstation",
+    link:
+      "https://www.amazon.com/DualSense-Wireless-Controller-PlayStation-5/dp/B08FC6C75Y/ref=lp_16225016011_1_6"
+  },
+  {
+    id: 7,
+    name: "PlayStation 4 Slim 1TB Console",
+    price: 381,
+    rating: 5,
+    image: 'assets/img/7.jpg',
+    description: "by Playstation",
+    link:
+      "https://www.amazon.com/PlayStation-4-Slim-1TB-Console/dp/B071CV8CG2/ref=lp_16225016011_1_9"
+  },
+  {
+    id: 8,
+    name: "Ring Fit Adventure - Nintendo Switch",
+    price: 73,
+    rating: 4.5,
+    image: 'assets/img/8.jpg',
+    description: "by Nintendo",
+    link:
+      "https://www.amazon.com/Ring-Fit-Adventure-Nintendo-Switch/dp/B07XV4NHHN/ref=lp_16225016011_1_7"
+  },
+  {
+    id: 9,
+    name: "Animal Crossing: New Horizons - Nintendo Switch",
+    price: 45,
+    rating: 5,
+    image: 'assets/img/9.jpg',
+    description: "by Nintendo",
+    link:
+      "https://www.amazon.com/Animal-Crossing-New-Horizons-Nintendo-Switch/dp/B07SL6ZXBL/ref=lp_16225016011_1_8"
+  },
+
+  {
+    id: 10,
+    name: "Just Dance 2021 - Nintendo Switch Standard Edition",
+    price: 30,
+    rating: 5,
+    image: 'assets/img/10.jpg',
+    description: "by Ubisoft",
+    link:
+        "https://www.amazon.com/Just-Dance-2021-Nintendo-Switch-Standard/dp/B08BRQKSMB/ref=lp_16225016011_1_11"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
